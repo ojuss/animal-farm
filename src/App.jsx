ae5a383ff59c6eab1c5002a3d49b9b1050c05cd2
Ojus <guptaojas75@gmail.com>
2024-10-26 20:07:13 +0530

    add caching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [animals, setAnimals] = useState([]);
+  useEffect(()=> {
+    const lastQuery = localStorage.getItem('lastQuery')
+    search(lastQuery)
+  }, [])
 
   const search = async (q) => {
     const response = await fetch(
@@ -11,6 +15,8 @@ function App() {
 
     const data = await response.json();
     setAnimals(data);
+
+    localStorage.setItem("lastQuery", q)
   };
 
   return (
